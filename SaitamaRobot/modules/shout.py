888928ef257cc8c88e20920681ee92cab727f76a
dizzorepocu <67877766+dizzorepocu@users.noreply.github.com>
2020-07-07 03:30:12 +0300

    Update shout.py

--- SaitamaRobot/modules/shout.py
+++ SaitamaRobot/modules/shout.py
@@ -22,11 +22,11 @@ def shout(bot: Bot, update: Update, args: List[str]):
 
 
 __help__ = """
- A little piece of fun wording! Give a loud shout out in the chatroom.
+ Eğlenceli bir parça küçük ifade! Sohbet odasında yüksek sesle bağır.
  
- i.e `/shout HELP`, bot replies with huge coded *HELP* letters within the square. 
+ i.e `/shout HELP`, bot kare içinde büyük kodlu * YARDIM * harfleri ile cevaplar. 
  
- • `/shout <keyword>`*:* write anything you want to give loud shout.
+ • `/shout <klavye>`*:* yüksek sesle vermek istediğiniz her şeyi yazın.
     ```
     t e s t
     e e
