6fad8442ee460e7704f3f076cf7e956fd497fdb6
dizzorepocu <67877766+dizzorepocu@users.noreply.github.com>
2020-07-08 02:22:20 +0300

    Update blacklist_stickers.py

--- SaitamaRobot/modules/blacklist_stickers.py
+++ SaitamaRobot/modules/blacklist_stickers.py
@@ -326,8 +326,7 @@ def del_blackliststicker(bot: Bot, update: Update):
 					message.delete()
 					mutetime = extract_time(message, value)
 					bot.restrict_chat_member(chat.id, user.id, until_date=mutetime, can_send_messages=False)
-					bot.sendMessage(chat.id, " bu kişi {} için susturuldu çünkü kara liste etiketlerindeki '{}'.format(mention_markdown(user.id, user.first_name), value, trigger), parse_mode="markdown")
-					return
+					bot.sendMessage(chat.id, "{} sesi kapatıldı {} çünkü kullanmak '{}' hangi kara liste çıkartmaları".format(mention_markdown(user.id, user.first_name), value, trigger), parse_mode="markdown")
 			except BadRequest as excp:
 				if excp.message == "Silinecek mesaj bulunamadı":
 					pass
